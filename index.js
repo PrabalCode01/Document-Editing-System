22072050c02f2efd21a5e08af88a387e04dede04
Prabal singh <99129201+PrabalCode01@users.noreply.github.com>
2024-01-25 15:26:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-import { Server } from "socket.io";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-import Connection from "./databases/db.js";
-
-const PORT = 9000
-
-Connection();
-
-import { getDocument,updateDocument } from "./Controller/documentController.js";
-
-const io = new Server(PORT,{
-    cors:{
-            origin:'http://localhost:3000',
-            methods:['GET','POST']
-    }
-});
-
-io.on('connection', socket =>{
-
-    socket.on('get-document',async documentId =>{
-     
-        const document = await getDocument(documentId);
-        socket.join(documentId)
-        socket.emit('load-document',document.data);
-
-        socket.on('send-changes',delta=>{
-           socket.broadcast.to(documentId).emit('receive-changes',delta);
-        })
-
-        socket.on('save-document',async data =>{
-            await updateDocument(documentId,data);
-        })
-    })
-
-});
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
